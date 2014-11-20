eca87548f3ab33872a055f4d916e4b0cb8bdf4c3
Morten Bøgh <mob@trifork.com>
2014-11-20 08:43:51 +0100

    Initial commit

---
+++ Janitor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Janitor
+//
+//  Created by Morten Bøgh on 20/11/14.
+//  Copyright (c) 2014 Just A Beech. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
