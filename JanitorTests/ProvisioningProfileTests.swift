e40c7b263e069955953f7c55cbe8dac5fb6cf6cf
Morten Bøgh <mob@trifork.com>
2014-11-24 19:56:14 +0100

    Testing on real mobileprovision file

--- JanitorTests/ProvisioningProfileTests.swift
+++ JanitorTests/ProvisioningProfileTests.swift
@@ -24,4 +24,21 @@ class ProvisioningProfileTests: XCTestCase {
         let profile = ProvisioningProfile(filePath: filePath)
         XCTAssert(profile == nil, "Initializer should return nil when passed an empty file path.")
     }
+
+    func testFilenameAndFilePath() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let filePath = bundle.pathForResource("Total_Recall_Development", ofType: "mobileprovision")
+        let profile = ProvisioningProfile(filePath: filePath!)
+        XCTAssert(profile?.filePath == filePath, "Filepath should match the filepath used to initialize the object.")
+        XCTAssert(profile?.filename == filePath?.lastPathComponent, "Filename should match the last path component of the filePath")
+    }
+
+    func testProfileName() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let filePath = bundle.pathForResource("Total_Recall_Development", ofType: "mobileprovision")
+        let profile = ProvisioningProfile(filePath: filePath!)
+        XCTAssert(profile?.name == "Total Recall Development", "Name should match that of the profile :)")
+    }
+
+    
 }
